b67f28ee5b6fc2a228d0b3f32b2013d8029ec863
叶晨昊 <126464945+ychAlbert@users.noreply.github.com>
2023-07-12 14:09:53 +0100

    Create lab03.R

---
+++ lab03.R
@@ -0,0 +1,7 @@
+print("I have created my first repository!")
+
+print("My name on GitHub is set to:")
+system("git config user.name")
+
+print("My e-mail on GitHub is set to:")
+system("git config user.email")
